c6ccdc46eb6394d4ac321095c18f34974f070eaa
Christian Bromann <git@bromann.dev>
2023-04-19 17:27:05 -0700

    fix cjs tests

--- src/cjs/index.ts
+++ src/cjs/index.ts
@@ -1,6 +1,16 @@
-exports.run = async function (markdownFilePath: any, idOrIds: any, args: any) {
+exports.run = async function (markdownFilePath: any, id: any, args: any) {
   const { run } = await import('../index.js')
-  return run(markdownFilePath, idOrIds, args)
+  return run(markdownFilePath, id, args)
+}
+
+exports.runSeries = async function (markdownFilePath: any, idOrIds: any, args: any) {
+  const { runSeries } = await import('../index.js')
+  return runSeries(markdownFilePath, idOrIds, args)
+}
+
+exports.runParallel = async function (markdownFilePath: any, idOrIds: any, args: any) {
+  const { runParallel } = await import('../index.js')
+  return runParallel(markdownFilePath, idOrIds, args)
 }
 
 exports.createServer = async function (serverAddress: any, args: any) {
