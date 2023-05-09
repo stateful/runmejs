e58c5507db9ba319742f436ac8bd1f74b87be19d
Christian Bromann <git@bromann.dev>
2023-05-09 10:31:22 -0700

    add process to download zip

--- src/installer.ts
+++ src/installer.ts
@@ -3,11 +3,13 @@ import url from 'node:url'
 import util from 'node:util'
 import path from 'node:path'
 import zlib from 'node:zlib'
-import fs from 'node:fs/promises'
-import stream from 'node:stream'
+import fs from 'node:fs'
+import fsp from 'node:fs/promises'
+import stream, { Readable } from 'node:stream'
 
 import tar from 'tar-fs'
 import fetch from 'node-fetch'
+import unzipper from 'unzipper'
 
 import { hasAccess } from './utils.js'
 import { SUPPORTE_PLATFORMS } from './constants.js'
@@ -19,10 +21,11 @@ const streamPipeline = util.promisify(stream.pipeline)
  * download runme binary from GitHub
  * @returns file path to downloaded binary
  */
-export async function download (runmeVersion = process.env.RUNME_VERSION || 'latest') {
+export async function download(runmeVersion = process.env.RUNME_VERSION || 'latest') {
   const targetDir = path.resolve(__dirname, '..', '.bin')
   const isWindows = os.platform() === 'win32'
-  const binaryFilePath = path.resolve(targetDir, `runme${isWindows ? '.exe' : ''}`)
+  const binaryName = 'runme' + isWindows ? '.exe' : ''
+  const binaryFilePath = path.resolve(targetDir, binaryName)
 
   if (await hasAccess(binaryFilePath)) {
     return binaryFilePath
@@ -41,7 +44,13 @@ export async function download (runmeVersion = process.env.RUNME_VERSION || 'lat
     throw new Error(`Failed to download binary (statusCode ${res.status})`)
   }
 
-  await fs.mkdir(targetDir, { recursive: true })
+  await fsp.mkdir(targetDir, { recursive: true })
+
+  if (ext === 'zip') {
+    await downloadZip(res.body, binaryFilePath)
+    return binaryFilePath
+  }
+
   await streamPipeline(res.body, zlib.createGunzip(), tar.extract(targetDir))
   return binaryFilePath
 }
@@ -68,4 +77,28 @@ const getPlatformMetadata = () => {
     `by "runme".\nYour system must be one of the following:\n`
   )
   console.table(SUPPORTE_PLATFORMS)
+}
+
+function downloadZip(body: NodeJS.ReadableStream, binaryPath: string) {
+  const stream = Readable.from(body).pipe(unzipper.Parse())
+  const promiseChain: Promise<void>[] = [
+    new Promise((resolve, reject) => {
+      stream.on('close', () => resolve())
+      stream.on('error', () => reject())
+    })
+  ]
+
+  stream.on('entry', (entry) => {
+    const fileName = entry.path
+    const type = entry.type
+    if (type === 'File' && path.extname(fileName) === '.exe') {
+      const execStream = entry.pipe(fs.createWriteStream(binaryPath))
+      promiseChain.push(new Promise((resolve, reject) => {
+        execStream.on('close', () => resolve())
+        execStream.on('error', reject)
+      }))
+    }
+  })
+
+  return Promise.all(promiseChain)
 }
