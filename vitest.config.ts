b9f6bb6ec15e6e0f75536c389e6cc8991791e6bd
Christian Bromann <git@bromann.dev>
2023-06-12 18:06:10 +0200

    auto restore mocks

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,20 +2,21 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    test: {
-        include: ['tests/*.test.ts'],
-        /**
-         * not to ESM ported packages
-         */
-        exclude: [
-            'tests/cjs',
-            'dist', '.idea', '.git', '.cache',
-            '**/node_modules/**'
-        ],
-        testTimeout: 30 * 1000,
-        watch: false,
-        coverage: {
-            enabled: false
-        }
+  test: {
+    include: ['tests/*.test.ts'],
+    /**
+     * not to ESM ported packages
+     */
+    exclude: [
+      'tests/cjs',
+      'dist', '.idea', '.git', '.cache',
+      '**/node_modules/**'
+    ],
+    testTimeout: 30 * 1000,
+    watch: false,
+    restoreMocks: true,
+    coverage: {
+      enabled: false
     }
+  }
 })
